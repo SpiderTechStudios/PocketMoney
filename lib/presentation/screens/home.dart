f20ccf2423803b351b39e84e5ce879e5766c1ffe
juku001 <jumakassim89@gmail.com>
2026-08-17 11:08:26 +0300

    v1.0.0:setup

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -5,6 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home Screen')));
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(child: Text('Site Under Management!', style: TextStyle(
+        color : Colors.white, 
+        fontSize: 40
+      ),)));
   }
 }
